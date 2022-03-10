ca10282e43bc3f22bec2fec4490293ff7420834b
Adam Scarberry <adam.scarberry@usace.army.mil>
2022-03-10 00:05:50 -0500

    add desktop icon, icons fixes for windows tray, excess process fix when restoring window

--- main.js
+++ main.js
@@ -7,6 +7,78 @@ const {
   ipcMain,
 } = require("electron");
 const http = require("http");
+const path = require('path');
+
+
+
+// https://github.com/electron/windows-installer/blob/0336cc646af849f125979ba623efdf5440852c4e/README.md#handling-squirrel-events
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function (command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
+    } catch (error) { }
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function (args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
 
 class AuthManager {
   constructor(config) {
@@ -17,7 +89,7 @@ class AuthManager {
     this.username = null;
 
     this.loggedOutIcon = config.loggedOutIcon;
-    this.loggedInIcon = nativeImage.createFromPath("./img/shield-check.png");
+    this.loggedInIcon = nativeImage.createFromPath(path.join(__dirname, "img", "shield-check-green.png"));
 
     this.ipc = ipcMain;
 
@@ -49,6 +121,11 @@ class AuthManager {
   }
 
   createWindow() {
+    if (this.win) {
+      //don't create another window (process) if existing window is hidden
+      this.win.show();
+      return false;
+    }
     this.win = new BrowserWindow({
       width: 800,
       height: 600,
@@ -64,6 +141,11 @@ class AuthManager {
     this.win.on("minimize", (event) => {
       event.preventDefault();
       this.win.hide();
+    });
+
+    this.win.on("activate", (event) => {
+      event.preventDefault();
+      this.win.show();
     });
 
     this.win.on("close", (event) => {
@@ -84,7 +166,7 @@ class AuthManager {
     this.updateContextMenu();
     this.updateTrayIcon();
     // reply to the ui that we're good
-    event.sender.send("asynchronous-reply", {status:200,username:this.username});
+    event.sender.send("asynchronous-reply", { status: 200, username: this.username });
   }
 
   requestListener(req, res) {
@@ -103,10 +185,14 @@ class AuthManager {
   }
 
   startup() {
+    if (!app.requestSingleInstanceLock()) {
+      this.quit()
+    }
     this.tray = new Tray(this.loggedOutIcon);
     this.updateContextMenu();
     this.startHttpServer();
     this.ipc.on("asynchronous-message", this.handleIpcMessage);
+    this.createWindow()
   }
 
   quit() {
@@ -130,7 +216,7 @@ class AuthManager {
 
 app.whenReady().then(() => {
   const auth = new AuthManager({
-    loggedOutIcon: nativeImage.createFromPath("./img/shield-key.png"),
+    loggedOutIcon: nativeImage.createFromPath(path.join(__dirname, "img", "shield-need-key.png")),
   });
   auth.startup();
 });
