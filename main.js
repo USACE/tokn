bb753e64bafe01476dc40bb158699c18c4e6fe51
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-03-15 16:01:41 -0400

    Feature/misc updates (#3)
    * app updates for windows
    
    * add desktop icon, icons fixes for windows tray, excess process fix when restoring window
    
    * name change
    
    * additional icons
    
    * loading screen to replace default
    
    * mac closing fixes
    
    * slight style changes
    
    * mac app icon
    
    * adjust tray icon, sizing for mac

--- main.js
+++ main.js
@@ -5,8 +5,78 @@ const {
   Menu,
   nativeImage,
   ipcMain,
-} = require("electron");
-const http = require("http");
+} = require('electron');
+const http = require('http');
+const path = require('path');
+
+// https://github.com/electron/windows-installer/blob/0336cc646af849f125979ba623efdf5440852c4e/README.md#handling-squirrel-events
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function (command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function (args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+}
 
 class AuthManager {
   constructor(config) {
@@ -17,7 +87,9 @@ class AuthManager {
     this.username = null;
 
     this.loggedOutIcon = config.loggedOutIcon;
-    this.loggedInIcon = nativeImage.createFromPath("./img/shield-check.png");
+    this.loggedInIcon = nativeImage.createFromPath(
+      path.join(__dirname, 'img', 'coin-16x16.png')
+    );
 
     this.ipc = ipcMain;
 
@@ -37,41 +109,68 @@ class AuthManager {
   createContextMenu() {
     if (this.token) {
       return Menu.buildFromTemplate([
-        { label: `Logged in as: ${this.username}` },
-        { label: "Quit", click: this.quit },
+        { label: `Logged in as: ${this.username}`, click: this.createWindow },
+        { label: 'Quit', click: this.quit },
       ]);
     } else {
       return Menu.buildFromTemplate([
-        { label: "Login", click: this.createWindow },
-        { label: "Quit", click: this.quit },
+        { label: 'Login', click: this.createWindow },
+        { label: 'Quit', click: this.quit },
       ]);
     }
   }
 
   createWindow() {
+    if (this.win) {
+      //don't create another window (process) if existing window is hidden
+      this.win.show();
+      return false;
+    }
     this.win = new BrowserWindow({
-      width: 800,
-      height: 600,
+      width: 600,
+      height: 400,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
       },
     });
 
-    this.win.loadFile("index.html");
+    this.win.loadFile('index.html');
     // this.win.webContents.openDevTools();
 
-    this.win.on("minimize", (event) => {
+    this.win.on('minimize', (event) => {
       event.preventDefault();
       this.win.hide();
     });
 
-    this.win.on("close", (event) => {
+    this.win.on('activate', (event) => {
+      event.preventDefault();
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      } else {
+        this.win.show();
+      }
+    });
+
+    this.win.on('close', (event) => {
       if (!app.isQuiting) {
         event.preventDefault();
         this.win.hide();
       } else {
-        console.log("quitting");
+        console.log('quitting');
+      }
+
+      return false;
+    });
+
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
+    this.win.on('window-all-closed', (event) => {
+      if (process.platform !== 'darwin') {
+        app.quit();
       }
 
       return false;
@@ -84,7 +183,10 @@ class AuthManager {
     this.updateContextMenu();
     this.updateTrayIcon();
     // reply to the ui that we're good
-    event.sender.send("asynchronous-reply", {status:200,username:this.username});
+    event.sender.send('asynchronous-reply', {
+      status: 200,
+      username: this.username,
+    });
   }
 
   requestListener(req, res) {
@@ -93,7 +195,7 @@ class AuthManager {
       res.end(this.token);
     } else {
       res.writeHead(404);
-      res.end("");
+      res.end('');
     }
   }
 
@@ -103,10 +205,14 @@ class AuthManager {
   }
 
   startup() {
+    if (!app.requestSingleInstanceLock()) {
+      this.quit();
+    }
     this.tray = new Tray(this.loggedOutIcon);
     this.updateContextMenu();
     this.startHttpServer();
-    this.ipc.on("asynchronous-message", this.handleIpcMessage);
+    this.ipc.on('asynchronous-message', this.handleIpcMessage);
+    this.createWindow();
   }
 
   quit() {
@@ -130,7 +236,9 @@ class AuthManager {
 
 app.whenReady().then(() => {
   const auth = new AuthManager({
-    loggedOutIcon: nativeImage.createFromPath("./img/shield-key.png"),
+    loggedOutIcon: nativeImage.createFromPath(
+      path.join(__dirname, 'img', 'coin-16x16-inactive.png')
+    ),
   });
   auth.startup();
 });
