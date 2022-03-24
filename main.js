0850f1ce2ece9c5f3a294f270d996ffa19671632
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-03-24 16:25:40 -0400

    auto update feature

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ const {
 const http = require('http');
 const path = require('path');
 
+// Only auto-update on Windows, Mac requires signed app
+if (process.platform === 'win32') {
+  require('update-electron-app')({
+    updateInterval: '1 hour',
+  });
+}
+
 // https://github.com/electron/windows-installer/blob/0336cc646af849f125979ba623efdf5440852c4e/README.md#handling-squirrel-events
 // this should be placed at top of main.js to handle setup events quickly
 if (handleSquirrelEvent()) {
@@ -34,7 +41,7 @@ function handleSquirrelEvent() {
 
     try {
       spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
-    } catch (error) { }
+    } catch (error) {}
 
     return spawnedProcess;
   };
@@ -217,8 +224,8 @@ class AuthManager {
     this.ipc.on('error', () => {
       // this will bring the app window to the foreground
       // and login will be preseted to user
-      app.relaunch()
-      app.exit()
+      app.relaunch();
+      app.exit();
     });
   }
 
